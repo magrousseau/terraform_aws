96911527a89f5bb0452b6ce3f6b017738b54fbf5
magrousseau <rousseaumagali@gmail.com>
2023-05-26 14:46:54 +0000

    Single virtual server configured

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+
+
 terraform {
   required_providers {
     aws = {
@@ -13,4 +15,28 @@ provider "aws" {
 
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+}
+
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "web-server-instance" {
+  ami               = data.aws_ami.ubuntu.id
+  instance_type     = "t2.micro"
+
+  user_data = "${file("install.sh")}"
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "web-server"
+  }
+
 }
